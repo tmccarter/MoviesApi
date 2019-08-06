783ea67333499507ae01e97239123c49e17d63ce
Tim McCarter <tmccarter@webmd.net>
2019-08-05 20:37:21 -0700

    added front-end

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+module.exports = {
+  entry: './wwwroot/source/app.js',
+  output: {
+    path: path.resolve(__dirname, 'wwwroot/dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
+};
---
+++ wwwroot/source/app.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MovieList from './movieList.jsx';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <MovieList url="/api/movies" />
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('container')
+);
